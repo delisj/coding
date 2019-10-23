a2c7d420d1284f9757d7b5146e34d145a947f0d2
delisj <delis.jaadla@gmail.com>
2019-10-23 17:01:35 +0300

    homework

---
+++ testike.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://api.chucknorris.io/jokes/random"
+
+r = requests.get(url)
+
+data = r.json()
+
+# print("id", data["id"])
+# print("icon_url", data["icon_url"])
+
+print(data['value'])
